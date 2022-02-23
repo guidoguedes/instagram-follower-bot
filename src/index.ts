c0ba71d3402c3c68d6b256f9398e30c0c217927f
guidoguedes <guidoguedes@gmail.com>
2022-02-23 18:23:09 +0000

    finally get data

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,24 @@ const app = express();
 const port = 8080 || process.env.PORT;
 const bot = new BotService();
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
 app.listen(port, async () => {
     await bot.login()
-    bot.run();
-  // tslint:disable-next-line:no-console
-  console.log(`server started at http://localhost:${port}`);
+    // bot.run();
+
+
+    // index page
+    app.get('/', function(req, res) {
+        res.render('pages/index');
+    });
+
+    // about page
+    // app.get('/about', function(req, res) {
+    //     res.render('pages/about');
+    // });
+
+    // tslint:disable-next-line:no-console
+    console.log(`server started at http://localhost:${port}`);
 });
--- src/services/bot.service.ts
+++ src/services/bot.service.ts
@@ -13,40 +13,13 @@ export default class BotService {
         this.ig = new IgApiClient();
     }
 
-    async run() {
-        const getWaitTime = () => (Math.random() * 5 * 60000) - (Math.random() * 1 * 60000);
-        setInterval(async () => {
-            if (!this.usersToFollow || this.usersToFollow.length < 1) {
-                console.log('getting latest post likers');
-                await this.getLatestPostLikers();
-            } else {
-                console.log('we have ' + this.usersToFollow.length + ' users we can follow');
-                let user = this.usersToFollow.pop();
-                while (user.is_private) {
-                    user = this.usersToFollow.pop();
-                }
-                console.log('attempting to following user: ', user.username);
-                await this.follow(user.pk);
-            }
-        }, getWaitTime())
-    }
-
-    async follow(userId: number) {
-        await this.ig.friendship.create(userId);
-    }
-
-    async getLatestPostLikers() {
-        const id = await this.ig.user.getIdByUsername(this.accountToParse);
-        const feed = await this.ig.feed.user(id);
-        const posts = await feed.items();
-        this.usersToFollow = await (await this.ig.media.likers(posts[0].id)).users;
-    }
-
     async login() {
         this.ig.state.generateDevice(this.user);
-        await this.ig.simulate.preLoginFlow();
+        // await this.ig.simulate.preLoginFlow(); generating 429 error too many redirects
         const loggedInAccount = await this.ig.account.login(this.user, this.password);
-        await this.ig.simulate.postLoginFlow();
-        console.log('logged in..');
+        // await this.ig.simulate.postLoginFlow(); generating 429 error too many redirects
+        // TODO send loggedInAccount data to index.ejs
+        // document.getElementById("ig_title").innerHTML = `logged user: ${JSON.stringify(loggedInAccount)} :)`;
+        // document.getElementById("ig_content").innerHTML = JSON.stringify(loggedInAccount);
     }
 }
